b0758af333f20f4706424cff9a67a86582f704b9
SN963 <72389122+SN963@users.noreply.github.com>
2021-06-14 14:16:31 +0200

    update in reserve

--- reserve.php
+++ reserve.php
@@ -1,13 +1,37 @@
 <?php
-  include 'Admin.php';
 require_once('database_Connect.php');
-  ?>
-  <head>
-    <link rel="stylesheet" type="text/css" href="reserve.css">
-  </head>
-  <body>
-    <form action="reserve.php" method="POST">
-            <table style = "border-collapse: collapse; width:50%; border:1px solid black;">
+include 'Admin.php';
+class reservation extends dbConnect
+{
+  public function display()
+  {
+    $sql="SELECT * FROM `booking` ";
+    $stmt=$this->connect()->query($sql);
+    while($row=$stmt->fetch())
+        {
+       print "<tr>";
+       print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['ID']."</td>";
+          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['FirstName']." ".$row['LastName']."</td>";
+          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['Time']."</td>";
+          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['City']."</td>";
+          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['Comment']."</td>";
+          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'><input type='checkbox' name='checkbox[]' value='".$row['ID']."'></td>";
+          print "</tr>";
+        }
+  }
+
+}
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="reserve.css">
+</head>
+<body>
+<center>
+  <form method="post" action="test2.php">
+      <div class='sub'>
+      <a href="" title="delete" class="delete" onclick="return confirm('Are you sure you want to delete this Reservartion')" style="position: absolute;"><input type= 'submit' name='delete' id='delete' value='Delete Records'></a></div>
+    <table style = "border-collapse: collapse; width:50%; border:1px solid black;">
       <tr> 
           <th style = 'text-align: center; font-size:20px; border:1px solid black;'> ID </th>
           <th style = 'text-align: center; font-size:20px; border:1px solid black;'> Name </th>
@@ -15,58 +39,12 @@ require_once('database_Connect.php');
           <th style = 'text-align: center; font-size:20px; border:1px solid black;'> City </th>
           <th style='text-align: center; font-size:20px; border:1px solid black;'>Comment</th>
           <th style = 'text-align: center; font-size:20px; border:1px solid black;'> Select </th>
-      </tr>
-    <?php
-    class reserve extends dbConnect
-    {
-      public function viewprofile()
-      {  
-        $sql="SELECT * FROM booking";
-        $stmt=$this->connect()->query($sql);
-        while($row=$stmt->fetch())
-        {
-    
-       print "<tr>";
-       print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['ID']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['FirstName']." ".$row['LastName']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['Time']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['City']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['Comment']."</td>";
-          print"<td style = 'text-align: center; font-size:20px; border:1px solid black;'><input type='checkbox' name='delete[]' value='<?= .$id. ?>'></td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'><input type='checkbox' id='checkItem' name='checkbox[]' value='<? =.$id.; ?>' ></td>";
-          print "</tr>";
-          
-          /*echo "<tr>";
-          echo "<td>".$row['ID']."</td>";
-          echo "<td><input type='checkbox' name=check[] value='".$row['ID']."'</td>";
-          echo "<tr>";*/
-
-        }
-        echo "<div class='sub'><input type= 'submit' name='delete' id='delete' value='Delete Records'>";
-        if(isset($_POST['delete']))
-          {
-            $checkbox = $_POST['check'];
-            foreach($checkbox as $id)
-            {
-              //$del_id = $checkbox[$i]; 
-               $dele="DELETE FROM `booking` where ID =".$id;
-                $r=$this->connect()->query($dele);
-                $message = "Data deleted successfully !";
-                echo $message;
-                echo $dele;
-            }
-      }
-
-      }
-    }
-    ?>
-    <?php
-    $get=new reserve();
-    $get->viewprofile();
-
-    ?>
-    </table>
+         </tr>
+  </form>
    
-        </div>
-     </form>
-  </body>
+</body>
+<?php
+$get=new reservation();
+$get->display();
+?>
+</html>
